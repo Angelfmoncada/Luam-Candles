6259bfc3cc8bc684b10073ab78c027a8330bfaf9
Angelfmoncada <angelfmoncada.m@gmail.com>
2025-08-03 13:35:09 -0600

    Initial commit: Luam Candles website

---
+++ proyectofinal.github.io-main/src/data/homeVideos.js
@@ -0,0 +1,24 @@
+import { asset } from '../utils/asset';
+
+export const homeVideos = [
+  { 
+    id: 1, 
+    title: 'Pedido Personalizado 1', 
+    src: asset('videos/video1.mp4')
+  },
+  { 
+    id: 2, 
+    title: 'Pedido Personalizado 2', 
+    src: asset('videos/video2.mp4')
+  },
+  { 
+    id: 3, 
+    title: 'Pedido Personalizado 3', 
+    src: asset('videos/video3.mp4')
+  },
+  { 
+    id: 4, 
+    title: 'Pedido Personalizado 4', 
+    src: asset('videos/video4.mp4')
+  }
+];
