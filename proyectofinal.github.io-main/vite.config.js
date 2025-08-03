18c435480a1fa2325d430d6500aa700dcefd6e8a
Angelfmoncada <angelfmoncada.m@gmail.com>
2025-08-03 13:41:32 -0600

    Fix build configuration and deploy to GitHub Pages

--- proyectofinal.github.io-main/vite.config.js
+++ proyectofinal.github.io-main/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/proyectofinal.github.io/',
+  base: '/Luam-Candles/',
   preview: {
     port: 4174,
     strictPort: true
